c060c89303f59257d69671e84c8020820e3f112b
EasterZhao <118179963+EasterZhao@users.noreply.github.com>
2022-12-01 01:13:09 +0000

    Door + Keypad

---
+++ Assets/Scripts/Doors.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doors : MonoBehaviour
+{
+    public Animator door;
+    public GameObject openText;
+    public bool inReach;
+
+
+
+
+    void Start()
+    {
+        inReach = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = true;
+            openText.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = false;
+            openText.SetActive(false);
+        }
+    }
+
+
+
+
+
+    void Update()
+    {
+
+        if (inReach && Input.GetKeyDown(KeyCode.E))
+        {
+            DoorOpens();
+        }
+
+        else
+        {
+            DoorCloses();
+        }
+
+
+
+
+    }
+    void DoorOpens ()
+    {
+        Debug.Log("It Opens");
+        door.SetBool("Open", true);
+        door.SetBool("Closed", false);
+    }
+
+    void DoorCloses()
+    {
+        Debug.Log("It Closes");
+        door.SetBool("Open", false);
+        door.SetBool("Closed", true);
+    }
+
+
+}
---
+++ Assets/Scripts/Keypad.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class Keypad : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject keypadOB;
+    public GameObject hud;
+    public GameObject inv;
+
+
+    public GameObject animateOB;
+    public Animator ANI;
+
+
+    public TMP_Text textOB;
+    public string answer = "12345";
+
+    public bool animate;
+
+
+    void Start()
+    {
+  
+
+    }
+    
+
+
+    public void Number(int number)
+    {
+        textOB.text += number.ToString();
+    }
+
+    public void Execute()
+    {
+        if (textOB.text == answer)
+        {
+            textOB.text = "Right";
+
+        }
+        else
+        {
+            textOB.text = "Wrong";
+        }
+
+
+    }
+
+    public void Clear()
+    {
+        {
+            textOB.text = "";
+        }
+    }
+
+    public void Exit()
+    {
+        keypadOB.SetActive(false);
+        inv.SetActive(true);
+        hud.SetActive(true);
+        Time.timeScale = 1f;
+    }
+
+    public void Update()
+    {
+        if (textOB.text == "Right" && animate)
+        {
+            ANI.SetBool("animate", true);
+            Debug.Log("its open");
+        }
+
+
+        if(keypadOB.activeInHierarchy)
+        {
+            hud.SetActive(false);
+            inv.SetActive(false);
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            Time.timeScale = 0f;
+        }
+
+    }
+}
