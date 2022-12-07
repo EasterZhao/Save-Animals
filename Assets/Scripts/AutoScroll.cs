d710bbec0a74543993cbdede0428c5cca2c60520
yolanda047 <yolandadada047@gmail.com>
2022-12-07 20:17:18 +0000

    Note&Text speed&Button UI

--- Assets/Scripts/AutoScroll.cs
+++ Assets/Scripts/AutoScroll.cs
@@ -5,7 +5,7 @@ using TMPro;
 //https://www.youtube.com/watch?v=9a6GyAbhLUk
 public class AutoScroll : MonoBehaviour
 {
-    float speed = 100.0f;
+    float speed = 40.0f;
     float textPosBegin = -695.0f;
     float boundaryTextEnd = 1036.0f;
 
